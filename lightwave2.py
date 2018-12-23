61daea3e133b8b00067c9c9e6dcdb419ffab7e2f
Bryan Blunt <bryan@blunt.me.uk>
2018-12-23 11:35:29 +0000

    Bump required lightwave2 version required to 0.0.9

--- lightwave2.py
+++ lightwave2.py
@@ -7,7 +7,7 @@ from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
 
 
-REQUIREMENTS = ['lightwave2==0.0.8']
+REQUIREMENTS = ['lightwave2==0.0.9']
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'lightwave2'
