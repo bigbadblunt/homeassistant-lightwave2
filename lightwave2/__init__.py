0dde10346ee62dbf2cf46241550ff78db39bd9ff
Bryan Blunt <bryan@blunt.me.uk>
2019-04-10 10:53:24 +0100

    Library version to 0.3.14

--- lightwave2/__init__.py
+++ lightwave2/__init__.py
@@ -5,7 +5,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
 from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
 
-REQUIREMENTS = ['lightwave2==0.3.13']
+REQUIREMENTS = ['lightwave2==0.3.14']
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'lightwave2'
