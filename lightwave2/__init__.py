73d1ba1a8f14cf36013229d872a1b8a30a1876de
Bryan Blunt <bryan@blunt.me.uk>
2019-06-08 22:43:44 +0100

    Public API

--- lightwave2/__init__.py
+++ lightwave2/__init__.py
@@ -3,22 +3,30 @@ import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
-from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
+from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD, CONF_API_KEY)
 
 REQUIREMENTS = ['lightwave2==0.3.22']
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'lightwave2'
+CONF_REFRESH_KEY = 'refresh_key'
+CONF_BACKEND = 'backend'
 LIGHTWAVE_LINK2 = 'lightwave_link2'
 LIGHTWAVE_BACKEND = 'lightwave_backend'
 BACKEND_EMULATED = 'emulated'
 BACKEND_PUBLIC = 'public'
 
 CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
+    DOMAIN: vol.Any({
         vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string
-    }
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_BACKEND): [BACKEND_EMULATED, BACKEND_PUBLIC]
+    },
+        {
+            vol.Required(CONF_API_KEY): cv.string,
+            vol.Required(CONF_REFRESH_KEY): cv.string,
+            vol.Optional(CONF_BACKEND): [BACKEND_EMULATED, BACKEND_PUBLIC]
+        }
     )
 }, extra=vol.ALLOW_EXTRA)
 
@@ -27,12 +35,11 @@ async def async_setup(hass, config):
     from lightwave2 import lightwave2
     _LOGGER.debug("Imported lightwave2 library version %s", REQUIREMENTS)
 
-    _tempbackend = BACKEND_PUBLIC #TODO - get this from config file
-
     email = config[DOMAIN][CONF_USERNAME]
     password = config[DOMAIN][CONF_PASSWORD]
 
-    if _tempbackend == BACKEND_EMULATED:
+    if CONF_BACKEND not in config[DOMAIN] or \
+            config[DOMAIN][CONF_BACKEND] == BACKEND_EMULATED:
         hass.data[LIGHTWAVE_BACKEND] = BACKEND_EMULATED
         link = lightwave2.LWLink2(email, password)
     else:
