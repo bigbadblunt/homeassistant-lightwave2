c319f0043ed2b576f95988ecb0d353c283e6debb
Bryan Blunt <bryan@blunt.me.uk>
2019-12-23 10:44:23 +0000

    Set up config flow

--- custom_components/lightwave2/config_flow.py
+++ custom_components/lightwave2/config_flow.py
@@ -8,6 +8,15 @@ class Lightwave2ConfigFlow(config_entries.ConfigFlow, domain='lightwave2'):
 
     async def async_step_user(self, user_input=()):
 
+        self._errors = {}
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+        if self.hass.data.get(DOMAIN):
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
         return self.async_show_form(
             step_id='user',
             data_schema=vol.Schema({
