712cd32680c54ce6a2a0c016962ef781bf262878
Bryan Blunt <bryan@blunt.me.uk>
2019-12-23 10:24:42 +0000

    Set up config flow

--- custom_components/lightwave2/config_flow.py
+++ custom_components/lightwave2/config_flow.py
@@ -1,7 +1,7 @@
 from homeassistant import config_entries
-from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD, CONF_API_KEY
+from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD, CONF_API_KEY)
 import voluptuous as vol
-#import homeassistant.helpers.config_validation as cv
+import homeassistant.helpers.config_validation as cv
 #from .const import DOMAIN #TODO move constants to const.py
 
 class Lightwave2ConfigFlow(config_entries.ConfigFlow, domain='lightwave2'):
@@ -11,6 +11,7 @@ class Lightwave2ConfigFlow(config_entries.ConfigFlow, domain='lightwave2'):
         return self.async_show_form(
             step_id='user',
             data_schema=vol.Schema({
-                vol.Required(CONF_USERNAME): str
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string
             })
         )
