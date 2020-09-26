67b9d857bf41dcecb0bd98d4622ab44de5ebfed4
Bryan Blunt <bryan@blunt.me.uk>
2020-09-26 10:51:13 +0100

    Allow multiple instances

--- custom_components/lightwave2/__init__.py
+++ custom_components/lightwave2/__init__.py
@@ -13,27 +13,30 @@ _LOGGER = logging.getLogger(__name__)
 
 async def handle_webhook(hass, webhook_id, request):
     """Handle webhook callback."""
-    link = hass.data[DOMAIN][config_entry.entry_id][LIGHTWAVE_LINK2]
-    body = await request.json()
-    _LOGGER.debug("Received webhook: %s ", body)
-    link.process_webhook_received(body)
-    for ent in hass.data[DOMAIN][LIGHTWAVE_ENTITIES]:
-        ent.async_schedule_update_ha_state(True)
+    for entry_id in hass.data[DOMAIN]:
+        link = hass.data[DOMAIN][entry_id][LIGHTWAVE_LINK2]
+        body = await request.json()
+        _LOGGER.debug("Received webhook: %s ", body)
+        link.process_webhook_received(body)
+        for ent in hass.data[DOMAIN][entry_id][LIGHTWAVE_ENTITIES]:
+            ent.async_schedule_update_ha_state(True)
 
 async def async_setup(hass, config):
     async def service_handle_led(call):
         entity_ids = call.data.get("entity_id")
-        entities = hass.data[DOMAIN][LIGHTWAVE_ENTITIES]
-        entities = [e for e in entities if e.entity_id in entity_ids]
-        rgb = call.data.get("rgb")
-        if str(rgb)[0:1] == "#":
-            rgb = int("0x" + rgb[1:7], 16)
-        else:
-            rgb = int(str(rgb), 0)
-        _LOGGER.debug("Received service call %s, rgb %s, rgb as hex %s", entity_ids, rgb, hex(rgb) )
-        for ent in entities:
-            _LOGGER.debug("Matched entites %s", ent)
-            await ent.async_set_rgb(led_rgb=rgb)
+        for entry_id in hass.data[DOMAIN]:
+            entities = hass.data[DOMAIN][entry_id][LIGHTWAVE_ENTITIES]
+            entities = [e for e in entities if e.entity_id in entity_ids]
+            link = hass.data[DOMAIN][entry_id][LIGHTWAVE_LINK2]
+            rgb = call.data.get("rgb")
+            if str(rgb)[0:1] == "#":
+                rgb = int("0x" + rgb[1:7], 16)
+            else:
+                rgb = int(str(rgb), 0)
+            _LOGGER.debug("Received service call %s, rgb %s, rgb as hex %s", entity_ids, rgb, hex(rgb) )
+            for ent in entities:
+                _LOGGER.debug("Matched entites %s", ent)
+                await ent.async_set_rgb(led_rgb=rgb)
 
     async def service_handle_lock(call):
         entity_ids = call.data.get("entity_id")
@@ -52,14 +55,18 @@ async def async_setup(hass, config):
 
     async def service_handle_unlock(call):
         entity_ids = call.data.get("entity_id")
-        entities = hass.data[DOMAIN][LIGHTWAVE_ENTITIES]
-        entities = [e for e in entities if e.entity_id in entity_ids]
+        for entry_id in hass.data[DOMAIN]:
 
-        for ent in entities:
-            feature_id = link.get_featureset_by_id(ent._featureset_id).features['protection'][0]
-            _LOGGER.debug("Received service call unlock")
-            _LOGGER.debug("Setting feature ID: %s ", feature_id)
-            await link.async_write_feature(feature_id, 0)
+            entities = hass.data[DOMAIN][entry_id][LIGHTWAVE_ENTITIES]
+            entities = [e for e in entities if e.entity_id in entity_ids]
+
+            link = hass.data[DOMAIN][entry_id][LIGHTWAVE_LINK2]
+
+            for ent in entities:
+                feature_id = link.get_featureset_by_id(ent._featureset_id).features['protection'][0]
+                _LOGGER.debug("Received service call unlock")
+                _LOGGER.debug("Setting feature ID: %s ", feature_id)
+                await link.async_write_feature(feature_id, 0)
 
     hass.services.async_register(DOMAIN, SERVICE_SETLEDRGB, service_handle_led)
     hass.services.async_register(DOMAIN, SERVICE_SETLOCKED, service_handle_lock)
@@ -68,10 +75,7 @@ async def async_setup(hass, config):
     return True
 
 async def async_setup_entry(hass, config_entry):
-    """Setup Lightwave hub. Uses undocumented websocket API."""
     from lightwave2 import lightwave2
-
-    
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN].setdefault(config_entry.entry_id, {})
