afba04d87343264ec42d94e20670f57aa8b5a9a2
Bryan Blunt <bryan@blunt.me.uk>
2021-10-10 12:56:38 +0100

    Add debug option

--- custom_components/lightwave2/__init__.py
+++ custom_components/lightwave2/__init__.py
@@ -110,11 +110,11 @@ async def async_setup_entry(hass, config_entry):
 
     if debugmode:
         _LOGGER.warning("Logging turned on")
-        hass.services.call(
-            "logger",
-            "set_level",
-            {"data": {"custom_components.lightwave2": "debug", "lightwave2.lightwave2": "debug"}}
-        )
+        #hass.services.call(
+        #    "logger",
+        #    "set_level",
+        #    {"data": {"custom_components.lightwave2": "debug", "lightwave2.lightwave2": "debug"}}
+        #)
 
     if not await link.async_connect(max_tries = 1):
         return False
