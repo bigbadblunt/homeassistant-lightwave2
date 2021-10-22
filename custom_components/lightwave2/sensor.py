9c03519afceef06cafe5c41100bf367c2d25ce49
Bryan Blunt <bryan@blunt.me.uk>
2021-10-22 19:05:41 +0100

    add signal and battery sensors

--- custom_components/lightwave2/sensor.py
+++ custom_components/lightwave2/sensor.py
@@ -66,12 +66,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
             for description in ENERGY_SENSORS:
                 sensors.append(LWRF2Sensor(name, featureset_id, link, url, description))
 
-    for featureset in link.featuresets:
-        if featureset.has_feature('rssi'):
+    for featureset_id, featureset in link.featuresets:
+        if  featureset.has_feature('rssi'):
             for description in SIGNAL_SENSORS:
                 sensors.append(LWRF2Sensor(featureset.name, featureset.featureset_id, link, url, description))
 
-    for featureset in link.featuresets:
+    for featureset_id, featureset in link.featuresets:
         if featureset.has_feature('batteryLevel'):
             for description in BATTERY_SENSORS:
                 sensors.append(LWRF2Sensor(featureset.name, featureset.featureset_id, link, url, description))
