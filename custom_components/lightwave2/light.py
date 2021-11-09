4c3141cfa8d38a652b4a1afe211beefa3b1f6055
Bryan Blunt <bryan@blunt.me.uk>
2021-11-09 19:19:40 +0000

    Tidy setled service call

--- custom_components/lightwave2/light.py
+++ custom_components/lightwave2/light.py
@@ -147,9 +147,6 @@ class LWRF2Light(LightEntity):
         self.async_schedule_update_ha_state()
 
     async def async_set_rgb(self, led_rgb):
-        self._r = led_rgb // 65536
-        self._g = (led_rgb - self._r * 65536) //256
-        self._b = (led_rgb - self._r * 65536 - self._g * 256)
         await self._lwlink.async_set_led_rgb_by_featureset_id(self._featureset_id, led_rgb)
 
     @property
