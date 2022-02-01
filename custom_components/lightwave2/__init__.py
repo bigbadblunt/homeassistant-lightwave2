c204d957502cf4102c4efd310d4f39ff79b75d86
Bryan Blunt <bryan@blunt.me.uk>
2022-02-01 17:54:27 +0000

    Test code for service call

--- custom_components/lightwave2/__init__.py
+++ custom_components/lightwave2/__init__.py
@@ -4,7 +4,7 @@ from .const import DOMAIN, CONF_PUBLICAPI, CONF_DEBUG, LIGHTWAVE_LINK2,  LIGHTWA
     LIGHTWAVE_WEBHOOK, LIGHTWAVE_WEBHOOKID, SERVICE_SETLEDRGB, SERVICE_SETLOCKED, SERVICE_SETUNLOCKED, \
     SERVICE_SETBRIGHTNESS, LIGHTWAVE_LINKID
 from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD)
-from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers import device_registry as dr, entity_platform
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,27 +20,30 @@ async def handle_webhook(hass, webhook_id, request):
 
 async def async_setup(hass, config):
 
-    async def service_handle_brightness(call):
-        entity_ids = call.data.get("entity_id")
-        _LOGGER.debug("Received service call set brightness %s", entity_ids)
+    async def service_handle_brightness(light, call):
+        #entity_ids = call.data.get("entity_id")
+        #_LOGGER.debug("Received service call set brightness %s", entity_ids)
         for entry_id in hass.data[DOMAIN]:
 
-            entities = hass.data[DOMAIN][entry_id][LIGHTWAVE_ENTITIES]
-            _LOGGER.debug("Brightness service call list of entities %s", entities)
-            _LOGGER.debug("Brightness service call list of entities %s", [e.entity_id for e in entities])
-            entities = [e for e in entities if e.entity_id in entity_ids]
-            _LOGGER.debug("Brightness service call list of entities 2 %s", entities)
+            #entities = hass.data[DOMAIN][entry_id][LIGHTWAVE_ENTITIES]
+            #_LOGGER.debug("Brightness service call list of entities %s", entities)
+            #_LOGGER.debug("Brightness service call list of entities %s", [e.entity_id for e in entities])
+            #entities = [e for e in entities if e.entity_id in entity_ids]
+            #_LOGGER.debug("Brightness service call list of entities 2 %s", entities)
             brightness = int(round(call.data.get("brightness") / 255 * 100))
 
             link = hass.data[DOMAIN][entry_id][LIGHTWAVE_LINK2]
 
-            for ent in entities:
-                feature_id = link.featuresets[ent._featureset_id].features['dimLevel'].id
-                _LOGGER.debug("Brightness service call setting feature ID: %s ", feature_id)
-                await link.async_write_feature(feature_id, brightness)
-                await ent.async_update()
+            #for ent in entities:
+            feature_id = link.featuresets[light._featureset_id].features['dimLevel'].id
+            #    _LOGGER.debug("Brightness service call setting feature ID: %s ", feature_id)
+            await link.async_write_feature(feature_id, brightness)
+            #await ent.async_update()
 
-    hass.services.async_register(DOMAIN, SERVICE_SETBRIGHTNESS, service_handle_brightness)
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(SERVICE_SETBRIGHTNESS, None,service_handle_brightness, )
+
+    #hass.services.async_register(DOMAIN, SERVICE_SETBRIGHTNESS, service_handle_brightness)
 
     return True
 
